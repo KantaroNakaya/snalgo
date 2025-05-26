006990839309529c6d7283781e56f00cff034a9a
KantaroNakaya <shimeno38saiko@gmail.com>
2025-05-26 10:03:10 +0900

    変換pythonファイル　CSVのヘッダー定義を変更

--- app/_libs/convert_json_to_csv.py
+++ app/_libs/convert_json_to_csv.py
@@ -2,13 +2,18 @@ import json
 import csv
 
 # JSONファイルを読み込む
-with open('app/_libs/algo_snap_questions.json', 'r', encoding='utf-8') as json_file:
+with open('work-data.json', 'r', encoding='utf-8') as json_file:
     data = json.load(json_file)
 
 # CSVファイルに書き込む
-with open('questions.csv', 'w', encoding='utf-8', newline='') as csv_file:
+with open('work-data.csv', 'w', encoding='utf-8', newline='') as csv_file:
     # CSVのヘッダーを定義
-    fieldnames = ['title', 'description', 'templateCode', 'answerCode01', 'answerCode02', 'answerCode03']
+    fieldnames = [
+        'title', 'description', 'templateCode',
+        'answer01_code', 'answer01_desc',
+        'answer02_code', 'answer02_desc',
+        'answer03_code', 'answer03_desc'
+    ]
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     
     # ヘッダーを書き込む

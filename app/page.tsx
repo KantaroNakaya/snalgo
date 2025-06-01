92544eb6e446f29affd97c62d9474ed2b8287f10
KantaroNakaya <shimeno38saiko@gmail.com>
2025-06-01 09:51:02 +0900

    トップページに問題例を追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
-import Image from "next/image";
-import { getNewsList } from "@/app/_libs/microcms";
-import { TOP_NEWS_LIMIT } from "@/app/_constants";
+import Workbook from "./_components/WorkList";
 import NewsList from "./_components/NewsList";
 import ButtonLink from "./_components/ButtonLink";
+import { getWorkList } from "@/app/_libs/microcms";
+import { getNewsList } from "@/app/_libs/microcms";
+import { TOP_NEWS_LIMIT } from "@/app/_constants";
 
 export const revalidate = 60;
 
-export default async function Home() {
-    const data = await getNewsList({
-        limit: TOP_NEWS_LIMIT,
-    });
+    export default async function Home() {
+        const workData = await getWorkList({
+            limit: TOP_NEWS_LIMIT,
+        });
+        const newsData = await getNewsList({
+            limit: TOP_NEWS_LIMIT,
+        });
     return (
         <div>
             <section className="flex items-center justify-center overflow-hidden py-32">
@@ -25,10 +29,17 @@ export default async function Home() {
                 </div>
             </section>
             <section className="max-w-3xl mx-auto px-10 py-6">
-                <h2 className="text-2xl">News</h2>
-                <NewsList news={data.contents} />
-                <div className="flex justify-end mt-4">
-                    <ButtonLink href="/news">もっとみる</ButtonLink>
+                <h2 className="text-3xl">問題例</h2>
+                <Workbook workbook={workData.contents} />
+                <div className="flex justify-end mt-8">
+                    <ButtonLink href="/workbook">問題一覧へ</ButtonLink>
+                </div>
+            </section>
+            <section className="max-w-3xl mx-auto px-10 py-6">
+                <h2 className="text-3xl">お知らせ</h2>
+                <NewsList news={newsData.contents} />
+                <div className="flex justify-end mt-8">
+                    <ButtonLink href="/news">お知らせ一覧へ</ButtonLink>
                 </div>
             </section>
         </div>

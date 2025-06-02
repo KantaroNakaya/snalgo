fa69ea57c7b721e5ce8eadf963fa04a6b26b11eb
KantaroNakaya <shimeno38saiko@gmail.com>
2025-06-02 11:10:35 +0900

    stateで答えの表示を切り替え

--- app/_components/AnswerCode.tsx
+++ app/_components/AnswerCode.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import dynamic from "next/dynamic";
 import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { useState } from "react";
 
 const SyntaxHighlighter = dynamic(
     () => import("react-syntax-highlighter").then((mod) => mod.Prism),
@@ -24,6 +25,8 @@ export default function AnswerCode({
     code,
     description,
 }: AnswerCodeProps) {
+    const [isOpen, setIsOpen] = useState(false);
+
     if (!code) return null;
 
     return (
@@ -31,16 +34,24 @@ export default function AnswerCode({
             <h2 className="text-xl font-bold mb-4">
                 解答コード{String(number).padStart(2, "0")}
             </h2>
-            <div className="mb-4 border-l-4 border-primary">
+            <div className="mb-8 border-l-4 border-primary">
                 <p className="flex items-center gap-1">
                     <Image src="/icon-hint.png" alt="" width={40} height={40} />
                     <span className="text-sm font-bold">ヒント</span>
                 </p>
                 <p className="pl-4 mt-4">{description}</p>
             </div>
-            <SyntaxHighlighter language="javascript" style={tomorrow}>
-                {cleanCode(code)}
-            </SyntaxHighlighter>
+            <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="bg-bg-sub text-text-sub mb-4 px-4 py-2 rounded-md"
+            >
+                {isOpen ? "答えを隠す" : "答えをみる"}
+            </button>
+            {isOpen && (
+                <SyntaxHighlighter language="javascript" style={tomorrow}>
+                    {cleanCode(code)}
+                </SyntaxHighlighter>
+            )}
         </div>
     );
 }

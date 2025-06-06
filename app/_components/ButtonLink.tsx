bde3d7c3ec8c6e1f34fccfa87529d1dc3b16fcca
KantaroNakaya <shimeno38saiko@gmail.com>
2025-06-06 09:51:45 +0900

    トップページ　テキストやイラストの追加

--- app/_components/ButtonLink.tsx
+++ app/_components/ButtonLink.tsx
@@ -7,7 +7,7 @@ export default function ButtonLink({ href, children }: Props) {
     return (
         <a
             href={href}
-            className="block bg-blue-700 text-white hover:bg-opacity-80 transition-all duration-300 py-2"
+            className="block w-80 bg-blue-700 text-white hover:bg-opacity-80 transition-all duration-300 py-2 text-center"
         >
             {children}
         </a>
--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,39 @@
-import Workbook from "./_components/WorkList";
-import NewsList from "./_components/NewsList";
 import ButtonLink from "./_components/ButtonLink";
-import { getWorkList } from "@/app/_libs/microcms";
-import { getNewsList } from "@/app/_libs/microcms";
-import { TOP_NEWS_LIMIT } from "@/app/_constants";
-
+import Image from "next/image";
 export const revalidate = 60;
 
-    export default async function Home() {
-        const workData = await getWorkList({
-            limit: TOP_NEWS_LIMIT,
-        });
-        const newsData = await getNewsList({
-            limit: TOP_NEWS_LIMIT,
-        });
+export default async function Home() {
     return (
         <div>
             <section className="flex items-center justify-center overflow-hidden py-32">
                 <div>
                     <h1 className="text-5xl text-center">
-                        <span className="block font-bold mb-4 text-2xl md:text-4xl">
-                            AlgoSnap（JavaScript編）
+                        <span className="block font-bold mb-4 text-6xl md:text-4xl">
+                            AlgoSnap
                         </span>
-                        <span className="block text-sm md:text-xl">
-                            スマホでサクッと学べるアルゴリズム基礎ドリル
+                        <span className="block text-xl md:text-xl">
+                            JavaScript編
                         </span>
                     </h1>
                 </div>
             </section>
-            <section className="max-w-3xl mx-auto px-10 py-6">
-                <h2 className="text-3xl">問題例</h2>
-                <Workbook workbook={workData.contents} />
-                <div className="flex justify-end mt-8">
-                    <ButtonLink href="/workbook">問題一覧へ</ButtonLink>
+            <section>
+                <p className="max-w-3xl mx-auto px-10 py-8 text-center">
+                    <span className="text-2xl font-bold">スマホでサクッと学べる</span><br />
+                    <span className="text-2xl font-bold">アルゴリズム基礎ドリル</span>
+                </p>
+                <div className="max-w-3xl mx-auto px-10 py-8">
+                    <Image src="/illust.png" alt="" width={300} height={300} />
+                </div>
+                <div className="flex flex-col gap-8 max-w-3xl mx-auto px-10 py-16">
+                    <p className="text-2xl font-bold">腰を据えてプログラムを考える。もちろんそれは大事なことであり、欠かせないことである。</p>
+                    <p className="text-2xl font-bold">しかし、腰を据えていない時間もある。その時間を有効活用したい。</p>
+                    <p className="text-2xl font-bold">そんな時におすすめなのが、AlgoSnapです。</p>
                 </div>
             </section>
-            <section className="max-w-3xl mx-auto px-10 py-6">
-                <h2 className="text-3xl">お知らせ</h2>
-                <NewsList news={newsData.contents} />
-                <div className="flex justify-end mt-8">
-                    <ButtonLink href="/news">お知らせ一覧へ</ButtonLink>
-                </div>
+            <section className="flex flex-col items-center gap-4 p-6">
+                <ButtonLink href="/workbook">問題一覧へ</ButtonLink>
+                <ButtonLink href="/news">お知らせ一覧へ</ButtonLink>
             </section>
         </div>
     );

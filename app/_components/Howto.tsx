d65128421690dce8bf3bf42680bda1b43778ba7b
KantaroNakaya <shimeno38saiko@gmail.com>
2025-06-15 16:05:03 +0900

    「ホーム画面に追加する方法」をアコーディオンで表示非表示させる

--- app/_components/Howto.tsx
+++ app/_components/Howto.tsx
@@ -1,50 +1,69 @@
-import Image from "next/image";
+"use client";
 
-export default function Hero() {
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Howto() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <section className="w-full mb-8 border border-border-light p-4 text-center">
+        <section className="w-full mb-16 border border-border-light p-4 text-center">
             <p className="mb-4 text-left">
                 こちらのページをホーム画面に追加しておくと、手軽に学習を続けられるようになります。
             </p>
-            <p className="mb-4 text-left">▼ホーム画面に追加する方法</p>
-            <div className="flex flex-col gap-4 mb-4">
-                <a
-                    className="flex items-center justify-between gap-2 bg-green-500 px-8 py-2 rounded-md"
-                    href="https://youtube.com/shorts/5z6h77x958M?si=dd_EKrmIarRyREKF"
-                    target="_blank"
-                    rel="noopener noreferrer"
+            <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="w-full text-left flex items-center justify-between bg-bg-sub2 px-4 py-2 rounded-md text-text-sub"
+            >
+                <p>ホーム画面に追加する方法</p>
+                <span
+                    className={`transform transition-transform ${
+                        isOpen ? "rotate-180" : ""
+                    }`}
                 >
-                    <span className="text-sm">AndroidOS版の場合</span>
-                    <span className="w-8 h-6">
-                        <Image
-                            src="/icon-youtube.png"
-                            alt="youtube"
-                            width={32}
-                            height={24}
-                        />
-                    </span>
-                </a>
-                <a
-                    className="flex items-center justify-between gap-2 bg-green-500 px-8 py-2 rounded-md"
-                    href="https://youtube.com/shorts/0pblK9BD6Ec?si=SCOVE-546iJAM-9O"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <span className="text-sm">iOS版の場合</span>
-                    <span className="w-8 h-6">
-                        <Image
-                            src="/icon-youtube.png"
-                            alt="youtube"
-                            width={32}
-                            height={24}
-                        />
-                    </span>
-                </a>
-            </div>
-            <ul className="text-left text-xs leading-relaxed">
-                <li>※ リンク先はオススメのショート動画です。</li>
-                <li>※ Snalgo運営による制作動画ではありません。</li>
-            </ul>
+                    ▼
+                </span>
+            </button>
+            {isOpen && (
+                <div className="flex flex-col gap-4 mb-4 pt-4">
+                    <a
+                        className="flex items-center justify-between gap-2 bg-green-500 px-8 py-2 rounded-md"
+                        href="https://youtube.com/shorts/5z6h77x958M?si=dd_EKrmIarRyREKF"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <span className="text-sm">AndroidOS版の場合</span>
+                        <span className="w-8 h-6">
+                            <Image
+                                src="/icon-youtube.png"
+                                alt="youtube"
+                                width={32}
+                                height={24}
+                            />
+                        </span>
+                    </a>
+                    <a
+                        className="flex items-center justify-between gap-2 bg-green-500 px-8 py-2 rounded-md"
+                        href="https://youtube.com/shorts/0pblK9BD6Ec?si=SCOVE-546iJAM-9O"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <span className="text-sm">iOS版の場合</span>
+                        <span className="w-8 h-6">
+                            <Image
+                                src="/icon-youtube.png"
+                                alt="youtube"
+                                width={32}
+                                height={24}
+                            />
+                        </span>
+                    </a>
+                    <ul className="text-left text-xs leading-relaxed">
+                        <li>※ リンク先はオススメのショート動画です。</li>
+                        <li>※ Snalgo運営による制作動画ではありません。</li>
+                    </ul>
+                </div>
+            )}
         </section>
     );
 }

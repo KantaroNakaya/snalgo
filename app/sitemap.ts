1f7bd79214dcfe4ce64b11a8b67a349f4e87e5fd
KantaroNakaya <shimeno38saiko@gmail.com>
2025-07-27 09:42:07 +0900

    sitemapの追記

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,36 +1,49 @@
-import { MetadataRoute } from "next";
-import { getAllNewsList } from "./_libs/microcms";
+import { MetadataRoute } from 'next';
+import { getAllNewsList } from './_libs/microcms';
+import { frameworkSlugMap } from './_libs/utils';
 
-const buildurl = (path?: string) => `https://snalgo.com${path ?? ""}`;
+const buildurl = (path?: string) => `https://snalgo.com${path ?? ''}`;
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap>{
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const newsContents = await getAllNewsList();
 
-    const newsUrls: MetadataRoute.Sitemap =
-    newsContents.map((content) => ({
+    const newsUrls: MetadataRoute.Sitemap = newsContents.map((content) => ({
         url: buildurl(`/news/${content.id}`),
         lastModified: content.revisedAt,
     }));
 
+    // フレームワークページのURLを生成
+    const frameworkUrls: MetadataRoute.Sitemap = Object.values(
+        frameworkSlugMap
+    ).map((slug) => ({
+        url: buildurl(`/workbook/framework/${slug}`),
+        lastModified: new Date(),
+    }));
+
     const now = new Date();
 
-    return[
+    return [
         {
             url: buildurl(),
             lastModified: now,
         },
         {
-            url: buildurl("/workbook"),
+            url: buildurl('/concept'),
             lastModified: now,
         },
         {
-            url: buildurl("/news"),
+            url: buildurl('/workbook'),
             lastModified: now,
         },
         {
-            url: buildurl("/contact"),
+            url: buildurl('/news'),
             lastModified: now,
         },
+        {
+            url: buildurl('/contact'),
+            lastModified: now,
+        },
+        ...frameworkUrls,
         ...newsUrls,
     ];
 }

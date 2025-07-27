0f05f3b79c55fba81d976f7e6267420996764794
KantaroNakaya <shimeno38saiko@gmail.com>
2025-07-27 09:58:59 +0900

    404ページを修正

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,18 @@
-import styles from './not-found.module.css';
-
 export default function NotFound() {
-  return (
-    <div className={styles.container}>
-      <dl>
-        <dt className={styles.title}>ページが見つかりませんでした</dt>
-        <dd className={styles.text}>
-          あなたがアクセスしようとしたページは存在しません。
-          <br />
-          URLを再度ご確認ください。
-        </dd>
-      </dl>
-    </div>
-  );
+    return (
+        <div className="py-20 px-10">
+            <dl>
+                <dt className="text-2xl text-center font-bold mb-6">
+                    ページが見つかりませんでした
+                </dt>
+                <dd className="text-lg text-center mb-10">
+                    あなたがアクセスしようとした
+                    <br />
+                    ページは存在しません。
+                    <br />
+                    URLを再度ご確認ください。
+                </dd>
+            </dl>
+        </div>
+    );
 }

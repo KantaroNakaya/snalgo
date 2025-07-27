c1cd7f2cd588ba6fae0d511e4e2d5a30e6de39f6
KantaroNakaya <shimeno38saiko@gmail.com>
2025-07-27 15:01:58 +0900

    404ページのデザインを変更

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,15 @@
+import Image from "next/image";
+
 export default function NotFound() {
     return (
-        <div className="py-20 px-10">
+        <div className="w-full h-screen flex flex-col items-center justify-center gap-10">
+            <Image
+                className="animate-head-wiggle"
+                src="/illust.png"
+                alt=""
+                width={300}
+                height={300}
+            />
             <dl>
                 <dt className="text-2xl text-center font-bold mb-6">
                     ページが見つかりませんでした
